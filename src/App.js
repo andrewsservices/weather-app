920454dd554c33803f9b791c33ab49d141e9b403
= <andylackk@outlook.com>
2022-11-16 12:44:41 -0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const [weatherData,setWeatherData] = useState([])
 
 
 useEffect(()=>{
+  console.log('useEffect')
   fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json')
   .then(res=>res.json())
   .then(data=>{
