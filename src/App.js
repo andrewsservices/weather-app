0d885eb231bc8f708eeb34b214a1fc5d686a48a0
= <andylackk@outlook.com>
2022-11-16 12:43:16 -0600

    data

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ useEffect(()=>{
   fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json')
   .then(res=>res.json())
   .then(data=>{
-    console.log(data)
+    console.log("data",data)
     const newData = data.dataseries
     const filterdData = newData.filter(ticker=>{
       if(Hourly.includes(ticker.timepoint)){
